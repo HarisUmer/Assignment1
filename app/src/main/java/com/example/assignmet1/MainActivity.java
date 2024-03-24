59cab76a509b190d198d409224c5441c6c9e07b1
HarisUmer <harisumer58@gmail.com>
2024-03-24 23:41:00 +0500

    2nd comit

--- app/src/main/java/com/example/assignmet1/MainActivity.java
+++ app/src/main/java/com/example/assignmet1/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.assignmet1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        t1 = findViewById(R.id.SignUp);
+        t1.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i1 = new Intent(MainActivity.this, Second_Screen.class);
+                        startActivity(i1);
+                    }
+                }
+        );
+
     }
 }
